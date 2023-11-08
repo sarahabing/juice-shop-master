08d2630b99951978d6231d52cfd2029cab0aceaa
sarahabing <abingsa1@gmail.com>
2023-11-08 16:28:34 -0700

    dunno

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -17,7 +17,7 @@ const password = "abcd@1234"
 
 fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
-      .then(json => console.log(password))
+      .then(json => console.log(json + " password ", password))
       
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err: Error) => console.log(err))
